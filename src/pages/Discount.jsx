eaa3794b3e4157625fa29db6afb959ba338d2401
Ali Nabeel <alinabeel.1980@gmail.com>
2023-05-17 03:19:05 +0300

    showing data in UI

--- src/pages/Discount.jsx
+++ src/pages/Discount.jsx
@@ -1,24 +1,46 @@
 import React from "react";
 import { useEffect,useState } from "react";
 import { db } from "../firebase";
-import { collection, getDocs}from "firebase/firestore";
+import { collection, query, onSnapshot}from "firebase/firestore";
+
 function Discount() {
 
     const [promoCode,setPromoCode]= useState([]);
 
-    const discountCollectionRef= collection(db,'Discounts')
-    useEffect(()=>{
-    const getPromoCode= async ()=>{
-    const data= await getDocs(discountCollectionRef);
-    console.log(data);
-    }
-    getPromoCode();
-    },[]);
+    useEffect(() => {
+        const discountCollectionRef = query(collection(db, 'Discounts'));
+        onSnapshot(discountCollectionRef, (querySnapshot) => {
+            setPromoCode(
+            querySnapshot.docs.map((doc) => ({
+            ...doc.data(),
+            Date: doc.data().Date.toDate().toLocaleString(),   
+            Exp: doc.data().Exp.toDate().toLocaleString(),
+            // unlike string and number, date type requires this function to be read by react
+            Permittivity: doc.data().Permittivity.path,
+            }))
+        );
+        });
+    }, []);
+    
 
     return(    
     <>
-    ghg
+    {promoCode.map((Discounts)=>{
+        return(
+            <>
+            
+            <h1>code:{Discounts.Code}</h1>
+            <h1>NumOfUse:{Discounts.NumOfUse}</h1>
+            <h1>DiscountValue:{Discounts.DiscountValue}</h1>
+            <h1>Date:{Discounts.Date}</h1>
+            <h1>Exp:{Discounts.Exp}</h1>
+            <h1>Permittivity:{Discounts.Permittivity}</h1>
+            
+            </>
+        )
+    })}
     </>
     )
 }
 export default Discount;
+
