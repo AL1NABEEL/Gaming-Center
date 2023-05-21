af06e96b6b4d45af0bfda8b6412a10cc799b647a
jawad <kadhimj161@gmail.com>
2023-05-21 09:45:45 +0300

    The first step of Login code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-
 import './App.css';
-
-
+import Login from './components/Login';
 function App() {
   return (
-    <>
- 
-    </>
+  <><Login/></>
+
+    
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value);
+    }
+  
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    }
+  
+    return (
+      <div className="login-form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-input-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" onChange={handleEmailChange} value={email} required />
+          </div>
+  
+          <div className="form-input-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" onChange={handlePasswordChange} value={password} required />
+          </div>
+  
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default LoginForm;
