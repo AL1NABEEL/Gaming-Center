509dd81e29416b8566506dea2504a238ff9f6abd
Melad duraid <meladhero4@gmail.com>
2023-06-01 10:54:19 +0300

    start with pc reservation

---
+++ src/component/PcVipData.jsx
@@ -0,0 +1,46 @@
+import vipChair from "../Makers project/logo and icons/vipChair.png"
+
+export const PcVipData = [
+
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair1" />,
+        Name : 'VIP-1'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair2" />,
+        Name : 'VIP-2'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair3" />,
+        Name : 'VIP-3'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair4" />,
+        Name : 'VIP-4'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair5" />,
+        Name : 'VIP-5'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair6" />,
+        Name : 'VIP-6'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair7" />,
+        Name : 'VIP-7'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair8" />,
+        Name : 'VIP-8'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair9" />,
+        Name : 'VIP-9'
+    },
+    {
+        icon : <img src={vipChair} width={60} height={60} alt="chair10" />,
+        Name : 'VIP-10'
+    },
+    
+    ]
--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ps5 from "../Makers project/logo and icons/ps5.png";
 import CountTime from '../component/CountTime'
 import {CountTimeData} from '../component/CountTimeData'
+import computer from "../Makers project/logo and icons/computer.png";
+import { PcVipData } from '../component/PcVipData';
 function Reservations() {
   return (
     <div className="Reservation-Container">
@@ -10,8 +12,9 @@ function Reservations() {
             <h1>Seat Reservations</h1>
             <p>PS5 , PC Desktop , other</p>
          </div>
-         <div className="Ps5-container">
-          <div className="ps5-logo">
+
+         <div className="Main-Container" id='ps5'>
+          <div className="LOGO">
             <img src={ps5} alt="ps5-logo" height={40} width={40}/>
             <p>Playstation Seats Reservations</p>
           </div>
@@ -20,6 +23,20 @@ function Reservations() {
           })}
           
          </div>
+
+         <div className='Main-Container' id='pc'>
+         <div className="LOGO">
+            <img src={computer} alt="computer" height={40} width={40}/>
+            <p>PC Seats Reservations</p>
+          </div>
+          {PcVipData.map((item,index)=>{
+            return(
+            <div key={index}>
+              <div className='PcVipChairs'>{item.icon}</div>
+            </div>)
+          })}
+         </div>
+
     </div>
   )
 }
