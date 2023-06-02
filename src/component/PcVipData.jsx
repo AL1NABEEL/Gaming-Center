d471a2ebb527acfced32240a1a8210f5dfce2fc7
Melad duraid <meladhero4@gmail.com>
2023-06-02 13:54:26 +0300

    PC section almost done

--- src/component/PcVipData.jsx
+++ src/component/PcVipData.jsx
@@ -1,46 +1,125 @@
 import vipChair from "../Makers project/logo and icons/vipChair.png"
-
+import chair2 from "../Makers project/logo and icons/chair2.png"
 export const PcVipData = [
 
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair1" />,
+        icon : <img src={vipChair} alt="chair1" />,
         Name : 'VIP-1'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair2" />,
+        icon : <img src={vipChair} alt="chair2" />,
         Name : 'VIP-2'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair3" />,
+        icon : <img src={vipChair} alt="chair3" />,
         Name : 'VIP-3'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair4" />,
+        icon : <img src={vipChair} alt="chair4" />,
         Name : 'VIP-4'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair5" />,
+        icon : <img src={vipChair} alt="chair5"/>,
         Name : 'VIP-5'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair6" />,
+        icon : <img src={vipChair} alt="chair6" />,
         Name : 'VIP-6'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair7" />,
+        icon : <img src={vipChair} alt="chair7" />,
         Name : 'VIP-7'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair8" />,
+        icon : <img src={vipChair} alt="chair8" />,
         Name : 'VIP-8'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair9" />,
+        icon : <img src={vipChair} alt="chair9" />,
         Name : 'VIP-9'
     },
     {
-        icon : <img src={vipChair} width={60} height={60} alt="chair10" />,
+        icon : <img src={vipChair} alt="chair10" />,
         Name : 'VIP-10'
     },
-    
+    {
+        icon : <img src={chair2} alt="chair1" />,
+        Name : 'STD-1'
+    },
+    {
+        icon : <img src={chair2} alt="chair2" />,
+        Name : 'STD-2'
+    },
+    {
+        icon : <img src={chair2} alt="chair3" />,
+        Name : 'STD-3'
+    },
+    {
+        icon : <img src={chair2} alt="chair4" />,
+        Name : 'STD-4'
+    },
+    {
+        icon : <img src={chair2} alt="chair5"/>,
+        Name : 'STD-5'
+    },
+    {
+        icon : <img src={chair2} alt="chair6" />,
+        Name : 'STD-6'
+    },
+    {
+        icon : <img src={chair2} alt="chair7" />,
+        Name : 'STD-7'
+    },
+    {
+        icon : <img src={chair2} alt="chair8" />,
+        Name : 'STD-8'
+    },
+    {
+        icon : <img src={chair2} alt="chair9" />,
+        Name : 'STD-9'
+    },
+    {
+        icon : <img src={chair2} alt="chair10" />,
+        Name : 'STD-10'
+    },
+    {
+        icon : <img src={chair2} alt="chair1" />,
+        Name : 'STD-11'
+    },
+    {
+        icon : <img src={chair2} alt="chair2" />,
+        Name : 'STD-12'
+    },
+    {
+        icon : <img src={chair2} alt="chair3" />,
+        Name : 'STD-13'
+    },
+    {
+        icon : <img src={chair2} alt="chair4" />,
+        Name : 'STD-14'
+    },
+    {
+        icon : <img src={chair2} alt="chair5"/>,
+        Name : 'STD-15'
+    },
+    {
+        icon : <img src={chair2} alt="chair6" />,
+        Name : 'STD-16'
+    },
+    {
+        icon : <img src={chair2} alt="chair7" />,
+        Name : 'STD-17'
+    },
+    {
+        icon : <img src={chair2} alt="chair8" />,
+        Name : 'STD-18'
+    },
+    {
+        icon : <img src={chair2} alt="chair9" />,
+        Name : 'STD-19'
+    },
+    {
+        icon : <img src={chair2} alt="chair10" />,
+        Name : 'STD-20'
+    }
     ]
--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import {React, useState} from 'react'
 import ps5 from "../Makers project/logo and icons/ps5.png";
 import CountTime from '../component/CountTime'
 import {CountTimeData} from '../component/CountTimeData'
 import computer from "../Makers project/logo and icons/computer.png";
 import { PcVipData } from '../component/PcVipData';
 function Reservations() {
+const [arrIndex, setarrIndex] = useState(0)
+
   return (
     <div className="Reservation-Container">
       
@@ -24,17 +26,43 @@ function Reservations() {
           
          </div>
 
+
+
+{/* Here is pc seat reservation section */}
+
          <div className='Main-Container' id='pc'>
          <div className="LOGO">
             <img src={computer} alt="computer" height={40} width={40}/>
             <p>PC Seats Reservations</p>
           </div>
+          <div className="pc-section">
+
+          <div className='PcVipChairs'>
           {PcVipData.map((item,index)=>{
             return(
-            <div key={index}>
-              <div className='PcVipChairs'>{item.icon}</div>
-            </div>)
+        
+              <button onClick={(e)=>{setarrIndex(arrIndex=>index)}} value={index} key={index}>
+              <div>{item.icon}</div>
+              <p >{item.Name}</p>
+            </button>
+            
+            )
           })}
+          </div>
+
+          <div className="pc-card">
+          {PcVipData.map((item,index)=>{
+            
+            return(
+            <div key={index}>
+              <div >{item.icon}</div>
+              <p >{item.Name}</p>
+            </div>)
+          })[arrIndex]}
+          </div>
+
+
+          </div>
          </div>
 
     </div>
