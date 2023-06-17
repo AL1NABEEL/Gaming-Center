17f4ca367c000dbc041d59117eceea6352cfc513
Melad duraid <meladhero4@gmail.com>
2023-06-17 15:40:41 +0300

    some styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='Dashboard-container'>
-      
+      <div className='top-section'>
+        <h1>Expand Creativity</h1>
+        <p>Collaborate, Streamline, Create</p>
+      </div>
     </div>
   )
 }
